93b004f8a71b1fcce5e0970066e607a2b643a3ae
Ayush Chugh <hey@ayushchugh.com>
2024-11-07 14:56:26 +0530

    feat: add logo upload functionality and improve invoice header UI

--- src/components/ui/invoice/InvoiceHeader.tsx
+++ src/components/ui/invoice/InvoiceHeader.tsx
@@ -2,6 +2,8 @@ import Typography from "@/components/ui/typography";
 import Image from "next/image";
 import { Input } from "../input";
 import type React from "react";
+import { FileIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 interface HeaderDetails {
   invoiceId: string;
@@ -15,7 +17,50 @@ interface InvoiceHeaderProps {
   setHeaderDetails: React.Dispatch<React.SetStateAction<HeaderDetails>>;
 }
 
-const InvoiceHeader = ({ headerDetails, setHeaderDetails }: InvoiceHeaderProps) => {
+const LogoPlaceholder: React.FC = () => {
+  return (
+    <div className="rounded border-2 border-dashed bg-white dark:bg-black w-32 h-32 flex items-center justify-center flex-col cursor-pointer transition-colors hover:bg-muted dark:hover:bg-neutral-950">
+      <FileIcon className="h-10 w-10 text-muted-foreground" />
+      <Typography className="text-muted-foreground">Logo</Typography>
+    </div>
+  );
+};
+
+const UploadLogo: React.FC = () => {
+  const [uploadedLogo, setUploadedLogo] = useState<string | null>(null);
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files?.[0]) {
+      setUploadedLogo(URL.createObjectURL(e.target.files[0]));
+    } else {
+      setUploadedLogo(null);
+    }
+  };
+
+  return (
+    <div>
+      <label htmlFor="logo-upload" className="cursor-pointer">
+        {uploadedLogo ? (
+          <Image src={uploadedLogo} alt="Logo" height={120} width={120} />
+        ) : (
+          <LogoPlaceholder />
+        )}
+      </label>
+      <Input
+        type="file"
+        id="logo-upload"
+        className="hidden"
+        onChange={onChangeHandler}
+        accept="image/png, image/jpg, image/jpeg"
+      />
+    </div>
+  );
+};
+
+const InvoiceHeader = ({
+  headerDetails,
+  setHeaderDetails,
+}: InvoiceHeaderProps) => {
   const onChangeHandler = (key: keyof HeaderDetails, value: string) => {
     setHeaderDetails((prev) => ({
       ...prev,
@@ -38,7 +83,7 @@ const InvoiceHeader = ({ headerDetails, setHeaderDetails }: InvoiceHeaderProps) 
           <Input
             value={headerDetails?.invoiceId}
             onChange={(e) => onChangeHandler("invoiceId", e.target.value)}
-            className="md:text-lg text-sm ml-[5rem] text-right border-none w-fit mt-2"
+            className="md:text-lg text-sm ml-[5rem] text-right border-none w-fit mt-2 bg-transparent"
           />
         </div>
 
@@ -50,7 +95,7 @@ const InvoiceHeader = ({ headerDetails, setHeaderDetails }: InvoiceHeaderProps) 
           <Input
             value={headerDetails?.invoiceDate}
             onChange={(e) => onChangeHandler("invoiceDate", e.target.value)}
-            className="md:text-lg text-sm ml-[5rem] text-right border-none w-fit mt-2"
+            className="md:text-lg text-sm ml-[5rem] text-right border-none w-fit mt-2 bg-transparent"
           />
         </div>
 
@@ -62,7 +107,7 @@ const InvoiceHeader = ({ headerDetails, setHeaderDetails }: InvoiceHeaderProps) 
           <Input
             value={headerDetails?.dueDate}
             onChange={(e) => onChangeHandler("dueDate", e.target.value)}
-            className="md:text-lg text-sm ml-[5rem] text-right border-none w-fit mt-2"
+            className="md:text-lg text-sm ml-[5rem] text-right border-none w-fit mt-2 bg-transparent"
           />
         </div>
 
@@ -73,19 +118,14 @@ const InvoiceHeader = ({ headerDetails, setHeaderDetails }: InvoiceHeaderProps) 
           <Input
             value={headerDetails?.paymentTerms}
             onChange={(e) => onChangeHandler("paymentTerms", e.target.value)}
-            className="md:text-lg text-sm ml-[5rem] text-right border-none w-fit mt-2"
+            className="md:text-lg text-sm ml-[5rem] text-right border-none w-fit mt-2 bg-transparent"
           />
         </div>
       </div>
       {/* Logo */}
       <div>
+        <UploadLogo />
         {/* TODO: Make this a placeholder instead of a hard coded logo */}
-        <Image
-          src={"/assets/logos/logo-icon-dark.svg"}
-          alt="Custom Logo"
-          height={120}
-          width={120}
-        />
       </div>
     </div>
   );
