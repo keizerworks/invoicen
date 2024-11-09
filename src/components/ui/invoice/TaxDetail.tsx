95bf4c9898645d8eec60cc6e89d735d154596a9f
pavanydg <pavanyermunja@gmail.com>
2024-11-10 00:14:19 +0530

    refactor: fixed error

--- src/components/ui/invoice/TaxDetail.tsx
+++ src/components/ui/invoice/TaxDetail.tsx
@@ -19,6 +19,7 @@ interface TaxDetailProps {
 
 const TaxDetail = ({ tax, index, taxDetails, updateTaxDetails }: TaxDetailProps) => {
   const { theme } = useTheme();
+  const [showDeleteButton, setShowDeleteButton] = useState(false);
   const handleChange = (field: keyof TaxDetail, value: string | number) => {
     const validatedValue = field === "percentage" ? Number(value) || 0 : value;
 
@@ -45,7 +46,7 @@ const TaxDetail = ({ tax, index, taxDetails, updateTaxDetails }: TaxDetailProps)
         <Input
           value={tax.description}
           onChange={(e) => handleChange("description", e.target.value)}
-          className={`border-none   focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-opacity-0`}
+          className={`border-none bg-white text-black"  dark:bg-black dark:text-white focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-opacity-0`}
           placeholder="Enter tax description..."
         />
       </TableCell>
@@ -55,7 +56,7 @@ const TaxDetail = ({ tax, index, taxDetails, updateTaxDetails }: TaxDetailProps)
           value={tax.percentage}
           onChange={(e) => handleChange("percentage", e.target.value)}
           min={0}
-          className={`border-none   focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-opacity-0 text-right`}
+          className={`border-none bg-white text-black"  dark:bg-black dark:text-white focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-opacity-0 text-right`}
         />
       </TableCell>
       <TableCell className="flex justify-end mt-1">
