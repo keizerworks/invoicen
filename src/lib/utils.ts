0ca79f2b748beadd974a44f70297f051f250213a
Mihir2423 <mihirraj444@gmail.com>
2024-11-15 02:52:36 +0530

    chore: update format utils funciton
    Ensure decimal formatting handles null and undefined values

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const formatDecimal = (value: number) => {
-  return value.toFixed(2);
+export const formatDecimal = (value: number): string => {
+ if (isNaN(value) || value === null || value === undefined) {
+   return "0.00";
+ }
+ const fixedValue = Math.abs(value) === 0 ? 0 : value;
+
+ return fixedValue.toFixed(2);
 };
