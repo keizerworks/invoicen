ddab3298e7dbbd91a672aca6099049ce417587b3
Mihir2423 <mihirraj444@gmail.com>
2024-11-15 02:48:41 +0530

    fix: total decimal places

--- src/components/ui/invoice/InvoiceFooter.tsx
+++ src/components/ui/invoice/InvoiceFooter.tsx
@@ -2,6 +2,7 @@ import Typography from "@/components/ui/typography";
 import { Button } from "../button";
 import { Textarea } from "@/components/ui/textarea";
 import { Separator } from "../separator";
+import { formatDecimal } from "@/lib/utils";
 
 interface InvoiceFooterProps {
   totalWithTax: number;
@@ -9,20 +10,20 @@ interface InvoiceFooterProps {
 
 const InvoiceFooter = ({ totalWithTax }: InvoiceFooterProps) => {
   return (
-    <div className="my-4 space-y-4 w-full ">
-      <div className="flex items-center justify-end w-full mt-4">
+    <div className="space-y-4 my-4 w-full">
+      <div className="flex justify-end items-center mt-4 w-full">
         <Typography variant="h3" className="font-semibold">
           Total:
         </Typography>
         <Typography variant="h3" className="ml-[1rem]">
           {/* TODO: format currency */}
-          {totalWithTax}
+          {formatDecimal(totalWithTax)}
         </Typography>
       </div>
       <Separator className="mt-4" />
       <Textarea
         placeholder="Add a custom message"
-        className="border-none shadow-none mt-4"
+        className="shadow-none mt-4 border-none"
       />
       <div className="flex justify-end">
         <Button className="text-right">Generate Invoice</Button>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const formatDecimal = (value: number) => {
+  return value.toFixed(2);
+};
