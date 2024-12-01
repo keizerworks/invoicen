675699b85f2621876dc01aafc9adfce2aa5fee87
arashveysi3 <am7team@gmail.com>
2024-12-02 01:18:07 +0330

    types modfied

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,15 +5,15 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 // this helper converts the numbers to any currency format for further use in project :)
-export function formatToCurrency(input:number, currencyType:string): string {
+export function formatToCurrency(input: number, currencyType: "USD" | "Euro" | "Rupee"): string {
   switch (currencyType) {
     case "USD":
-      return Intl.NumberFormat('en-US', { style: 'currency', currency: "USD" }).format(input,);
+      return Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(input);
     case "Euro":
-      return Intl.NumberFormat('de-DE', { style: 'currency', currency: "EUR" }).format(input,); 
+      return Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(input);
     case "Rupee":
-      return Intl.NumberFormat('en-IN', { style: 'currency', currency: "INR" }).format(input,);
+      return Intl.NumberFormat("en-IN", { style: "currency", currency: "INR" }).format(input);
     default:
-      return Intl.NumberFormat('en-US', { style: 'currency', currency: "USD" }).format(input,); 
+      return Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(input);
   }
 }
