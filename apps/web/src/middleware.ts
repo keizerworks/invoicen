200397d4157e8750e61c6c5aafba60a4ae36c654
Prithviraj-Indulkar <rindulkar2003@gmail.com>
2024-12-28 16:02:46 +0530

    refactor: seperate config file for storing various types of  routes

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,22 +1,17 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
+import { AUTH_ROUTES, PUBLIC_ROUTES } from "./routes.config";
+
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   const authToken = request.cookies.get("session")?.value ?? null;
 
-  if (
-    (pathname.startsWith("/signup") || pathname.startsWith("/signin")) &&
-    authToken
-  )
+  if (AUTH_ROUTES.includes(pathname) && authToken) {
     return NextResponse.redirect(new URL("/", request.url));
+  }
 
-  if (
-    !authToken &&
-    pathname !== "/signin" &&
-    pathname !== "/signup" &&
-    pathname !== "/forgot-password"
-  ) {
+  if (!authToken && !PUBLIC_ROUTES.includes(pathname)) {
     return NextResponse.redirect(new URL("/signin", request.url));
   }
   return NextResponse.next();
